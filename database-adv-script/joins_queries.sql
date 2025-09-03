ad8277b23ec89d03e3599ceaef28f1b7f6ae372f
Francis <kahotadfrajeh@gmail.com>
2025-09-03 13:16:45 +0300

    Create joins_queries.sql

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,16 @@
+--INNER JOIN
+SELECT *
+FROM Users
+INNER JOIN Bookings
+    ON Users.user_id = Bookings.user_id;
+
+-- LEFT JOIN 
+SELECT *
+FROM Properties
+LEFT JOIN Reviews
+    ON Properties.property_id = Reviews.property_id
+
+-- FULL OUTER JOIN 
+SELECT * FROM Users
+FULL OUTER JOIN Bookings
+    ON Users.user_id = Bookings.user_id;
