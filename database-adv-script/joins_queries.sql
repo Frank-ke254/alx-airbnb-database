49b04e2b3b8f5d86bb5656548537a1bbe7a65763
Francis <kahotadfrajeh@gmail.com>
2025-09-03 13:24:45 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -11,6 +11,8 @@ LEFT JOIN Reviews
     ON Properties.property_id = Reviews.property_id
 
 -- FULL OUTER JOIN 
-SELECT * FROM Users
-FULL OUTER JOIN Bookings
-    ON Users.user_id = Bookings.user_id;
+SELECT * 
+FROM Users
+UNION
+SELECT *
+FROM Bookings
